da465752d83d66be846929155968338abc3f07a6
Eric Maranga <ekirira22@gmail.com>
2024-04-11 09:42:39 +0300

    completed assessment

--- server/seed.py
+++ server/seed.py
@@ -10,14 +10,14 @@ with app.app_context():
     aloe = Plant(
         id=1,
         name="Aloe",
-        image="./images/aloe.jpg",
+        image="https://i.pinimg.com/originals/20/2f/51/202f515cfaceb0a5d6193117ea844205.gif",
         price=11.50,
     )
 
     zz_plant = Plant(
         id=2,
         name="ZZ Plant",
-        image="./images/zz-plant.jpg",
+        image="https://i.pinimg.com/originals/56/a6/14/56a614261d423da1825452363174c685.gif",
         price=25.98,
     )
 
